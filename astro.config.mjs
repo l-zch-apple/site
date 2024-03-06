6a62af43320e2447f72934790a8a5ba27428ee1b
l-zch <linzich3n@gmail.com>
2024-03-06 21:22:07 +0800

    add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind({
+    nesting: true
+  }), vue()],
+  server: {
+    host: "0.0.0.0"
+  }
 });
